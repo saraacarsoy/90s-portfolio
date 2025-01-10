0a39be5d62fc94aa2a7513bd4950ab101a332e29
sara <saraacarsoy@sabanciuniv.edu>
2025-01-10 19:33:46 +0100

    Add background image and text with absolute

--- src/routes/Popup.svelte
+++ src/routes/Popup.svelte
@@ -18,9 +18,15 @@
         <span>
           <u>S</u>ave
         </span>
-        <div class="content-info">
-          
-        </div>
+        <section class="content-info">
+          <span>About me</span>
+          <div class="content-text">Hi there! I’m Sara, an artsy programmer with almost 5 years of experience diving into all things code. My daily playgrounds are Java, JavaScript/TypeScript (hello Aurelia fans! *insert cricket chirp*), and PL/SQL. But when it comes to HTML and CSS, I’m all in.</div>
+          <div class="content-text">I graduated from Sabanci University in Istanbul with a degree in Computer Science and Engineering (50% scholarship, woo!) and later got my Master’s in Serious Games from the University of Skövde, also with a 50% scholarship. Currently, I’m working as a software consultant at Piktiv AB, where I’ve spent the last 3+ years helping a fintech company bring their ideas to life.</div>
+          <div class="content-text">When I’m not coding, you’ll probably find me crocheting, sketching some 2D illustrations, or fiddling with 3D modeling. Oh, and I am currently obsessed with Baldur’s Gate.</div>
+          <div class="side-text">So, welcome and hope you will like your stay!</div>
+
+          <img src="/background.png" class="about-image" alt="">
+        </section>
       </div>
     </div>
 </div>
@@ -69,9 +75,33 @@
   }
 
   .content-info {
+    position: relative;
     border: 0.5px solid #252525;
-    background-color: aquamarine;
-    min-height: 200px;
+    overflow-y: auto;
+    font-family: 'Courier New', Courier, monospace;
+    color: #000;
+    padding: 2rem;
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .about-image {
+    width: 100%;
+    position: absolute;
+    z-index: -1;
+    left: 0;
+    top: 0;
+  }
+
+  .content-text {
+    max-width: 650px;
+    margin-right: 37%;
+  }
+
+  .side-text {
+    max-width: 150px;
   }
 
   .close-button {
